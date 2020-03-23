54c988971decb529e97eecac5909ce796584ea26
chenling <chenling@deepexi.com>
2020-03-23 11:17:13 +0800

    修改数据

---
+++ docker.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+echo "add docker repo"
+tee /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/$releasever/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
+echo "------------"
+echo "update yum"
+yum update
+echo "------------"
+echo "install docker-engine"
+yum install -y docker-engine
+echo "------------"
+echo "install easy_install"
+wget -q http://peak.telecommunity.com/dist/ez_setup.py
+python ez_setup.py
+echo "------------"
+echo "install pip"
+easy_install pip
+echo "------------"
+echo "install docker-compose"
+pip install docker-compose
+echo "------------"
+echo "enable docker"
+systemctl enable docker.service
+sed '12c ExecStart=/usr/bin/dockerd --graph=/data/docker' /lib/systemd/system/docker.service
+echo "------------"
+echo "start docker"
+systemctl daemon-reload
+systemctl start docker
