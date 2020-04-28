8f65e53f383e7e3a441be521f41db2189c0ba94e
chenling <chenling@deepexi.com>
2020-04-28 17:59:18 +0800

    update config

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-echo "remove old docker "
 yum remove docker \
                   docker-client \
                   docker-client-latest \
@@ -11,27 +10,21 @@ yum remove docker \
                   docker-engine
 
 
-#echo "add docker repo"
-#tee /etc/yum.repos.d/docker.repo <<-'EOF'
-#[dockerrepo]
-#name=Docker Repository
-#baseurl=https://yum.dockerproject.org/repo/main/centos/$releasever/
-#enabled=1
-#gpgcheck=1
-#gpgkey=https://yum.dockerproject.org/gpg
-#EOF
-
-yum install -y yum-utils
-yum-config-manager \
-    --add-repo \
-    https://download.docker.com/linux/centos/docker-ce.repo
+tee /etc/yum.repos.d/docker.repo <<-'EOF'
+[dockerrepo]
+name=Docker Repository
+baseurl=https://yum.dockerproject.org/repo/main/centos/$releasever/
+enabled=1
+gpgcheck=1
+gpgkey=https://yum.dockerproject.org/gpg
+EOF
 
 
-echo "install docker-engine"
+
+
 yum install -y docker-engine
 
 
-echo "install docker-compose"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
@@ -52,7 +45,6 @@ tee /etc/docker/daemon.json <<-'EOF'
 }
 EOF
 
-echo "reStart docker"
 systemctl daemon-reload
 systemctl restart docker
 
